cd1e485936b3b9dbdb2f89d0de6b635a5d829802
Raymond Chavez <94435919+raymondchavez333@users.noreply.github.com>
2026-02-18 09:57:35 +0800

    Add the first case in switch statement

--- main.dart
+++ main.dart
@@ -11,7 +11,14 @@ void main(){
 
     stdout.write("Enter Choice: ");
     String choice = stdin.readLineSync() ?? '';
-    
+
+    switch(choice){
+
+      case '1':
+        stdout.write("Enter product name: ");
+        String productName = stdin.readLineSync() ?? '';
+        
+    }
 
   }
   while(choice != 4);
